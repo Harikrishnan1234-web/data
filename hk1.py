0dd48353852e33b4b19803d1fad186f803b3c1fb
Harikrishnan1234-web <138093604+Harikrishnan1234-web@users.noreply.github.com>
2026-05-05 09:23:33 +0530

    Update hk1.py

--- hk1.py
+++ hk1.py
@@ -1,3 +1,4 @@
+#3
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
@@ -19,3 +20,17 @@ plt.show()
 # Output
 print(m1.coef_[0][0])
 print(m2.intercept_[0], m2.coef_[0][0])
+
+
+#2
+import numpy as np, matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+np.random.seed(0)
+X = np.random.rand(100,1); y = 2 + 3*X + np.random.randn(100,1)
+
+m = LinearRegression().fit(X,y)
+
+print("Slope:", m.coef_[0][0], "Intercept:", m.intercept_[0])
+
+plt.scatter(X,y); plt.plot(X,m.predict(X)); plt.show()
