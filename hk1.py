fb875a7b2157829695191bfeea1561b49c211bcb
Harikrishnan1234-web <138093604+Harikrishnan1234-web@users.noreply.github.com>
2026-05-11 09:12:31 +0530

    Update hk1.py

--- hk1.py
+++ hk1.py
@@ -1,36 +1,15 @@
-#3
-import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
 
-# Data
-X = np.random.rand(100,1)
-y = 2 + 3*X + np.random.randn(100,1)
+df = pd.DataFrame({
+    'Status': ['Yes', 'No', None, 'Yes', 'No', 'Yes']
+})
 
-# Models
-m1 = LinearRegression(fit_intercept=False).fit(X,y)
-m2 = LinearRegression().fit(X,y)
+df['Status'] = df['Status'].fillna('No')
 
-# Plot
-plt.scatter(X,y)
-plt.plot(X, m1.predict(X),'r')
-plt.plot(X, m2.predict(X),'b')
+c = df['Status'].value_counts()
+print(c)
+
+c.plot(kind='pie', autopct='%1.1f%%')
+plt.title('Course Completion Status')
 plt.show()
-
-# Output
-print(m1.coef_[0][0])
-print(m2.intercept_[0], m2.coef_[0][0])
-
-
-#2
-import numpy as np, matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-
-np.random.seed(0)
-X = np.random.rand(100,1); y = 2 + 3*X + np.random.randn(100,1)
-
-m = LinearRegression().fit(X,y)
-
-print("Slope:", m.coef_[0][0], "Intercept:", m.intercept_[0])
-
-plt.scatter(X,y); plt.plot(X,m.predict(X)); plt.show()
